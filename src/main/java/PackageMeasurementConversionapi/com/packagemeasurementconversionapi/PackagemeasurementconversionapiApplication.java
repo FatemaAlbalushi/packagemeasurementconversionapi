f1d9adc03f1f2c0aac0d95572fb4ff4bf4365121
FatemaAlbalushi <fatoomsaid188@gmail.com>
2023-05-17 14:44:13 +0400

    API for Package Measurement Conversion

---
+++ src/main/java/PackageMeasurementConversionapi/com/packagemeasurementconversionapi/PackagemeasurementconversionapiApplication.java
@@ -0,0 +1,13 @@
+package PackageMeasurementConversionapi.com.packagemeasurementconversionapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackagemeasurementconversionapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PackagemeasurementconversionapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/PackageMeasurementConversionapi/com/packagemeasurementconversionapi/PackagemeasurementconversionapiApplicationTests.java
@@ -0,0 +1,13 @@
+package PackageMeasurementConversionapi.com.packagemeasurementconversionapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackagemeasurementconversionapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
